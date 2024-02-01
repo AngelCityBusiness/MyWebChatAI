f7759c61b4074ffe8785eebaaa814b8c36fe1632
AngelCityBusiness <158473688+AngelCityBusiness@users.noreply.github.com>
2024-02-01 05:16:24 -0800

    Update bot-10830.js

--- bot-10830.js
+++ bot-10830.js
@@ -1,7 +1,7 @@
 // bot-10830.js
 (function() {
     var script = document.createElement('script');
-    script.src = 'https://app.aminos.ai/js/chat_plugin.js'; // Original script source
+    script.src = 'https://app.aminos.ai/js/chat_plugin_ghl.js'; // Original script source
     script.setAttribute('data-bot-id', '10830'); // Setting the bot ID
 
     document.head.appendChild(script);
