a6c214b8b971d9eabc56954b98a16796d94f1eb0
AngelCityBusiness <158473688+AngelCityBusiness@users.noreply.github.com>
2024-02-01 21:31:17 -0800

    Update bot-10830.js
    Put custom CSS for text formatting so I don't have to give clients multiple codes (one for integration code and one for formatting).

--- bot-10830.js
+++ bot-10830.js
@@ -1,16 +1,28 @@
 // bot-10830.js
 (function() {
+    // Load the chat plugin
     var script = document.createElement('script');
     script.src = 'https://app.aminos.ai/js/chat_plugin_ghl.js'; // Original script source
     script.setAttribute('data-bot-id', '10830'); // Setting the bot ID
-
     document.head.appendChild(script);
+
+    // Add custom CSS styling
+    var css = `div.talktext p {
+        color: black !important;
+        font-family: sans-serif !important;
+    }`;
+    var style = document.createElement('style');
+    if (style.styleSheet) {
+        style.styleSheet.cssText = css;
+    } else {
+        style.appendChild(document.createTextNode(css));
+    }
+    document.head.appendChild(style);
 
     //PUT NEW INTEGRATION CODE IN COMMENT BELOW!!!
 
     //************************************************
     //NEW INTEGRATION CODE: <script src="https://www.angelcitymarketing.com/Integration-Code/bot-10830.js"></script>
-    //Don't forget to include ChatBot text formatting: <style> div.talktext p {color: black !important; font-family: sans-serif !important;} </style>
     //************************************************
 
     // Optionally, add any additional JavaScript code here
